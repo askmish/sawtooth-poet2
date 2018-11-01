347f2e69d1e4e6acf7568788601d46b13d83b51f
Rajeev Ranjan <rajeev2.ranjan@intel.com>
2018-11-01 14:53:52 +0530

    UTs for ConsensusStateStore & lmdb
    Updations in lmdb.rs. Implement new Unit Tests in ConsensusStateStore.
    
    Signed-off-by: Rajeev Ranjan <rajeev2.ranjan@intel.com>

--- src/core/src/consensus_state_store.rs
+++ src/core/src/consensus_state_store.rs
@@ -18,8 +18,9 @@
 use protobuf;
 use consensus_state::ConsensusState;
 use sawtooth_sdk::consensus::engine::BlockId;
+use database::lmdb;
 use database::lmdb::{LmdbContext, LmdbDatabase};
-use database::DatabaseError;
+use database::{DatabaseError, CliError};
 use bincode::{serialize, deserialize};
 
 #[derive(Debug)]
@@ -57,7 +58,7 @@ impl<'a> ConsensusStateStore<'a> {
     pub fn delete(&mut self, block_id: BlockId) -> Result<(), DatabaseError>{
         let mut writer = self.consensus_state_db.writer()?;
         writer.delete(&Vec::from(block_id))?;
-
+        writer.commit().unwrap();
         Ok(())
     }
 
@@ -67,6 +68,129 @@ impl<'a> ConsensusStateStore<'a> {
             DatabaseError::WriterError(format!("Failed to serialize state: {}", err))
         })?;
         writer.put(&Vec::from(block_id), &serialized_state)?;
+        writer.commit().unwrap();
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use database::config;
+
+    /// Asserts that there are COUNT many objects in DB.
+    fn assert_database_db_count(count: usize, db: &LmdbDatabase) {
+        let reader = db.reader().unwrap();
+
+        assert_eq!(reader.count().unwrap(), count,);
+    }
+
+    /// Asserts that there are are COUNT many objects in DB's INDEX.
+    fn assert_index_count(index: &str, count: usize, db: &LmdbDatabase) {
+        let reader = db.reader().unwrap();
+
+        assert_eq!(reader.index_count(index).unwrap(), count,);
+    }
+
+    /// Asserts that KEY is associated with VAL in DB.
+    fn assert_key_value(key: u8, val: u8, db: &LmdbDatabase) {
+        let reader = db.reader().unwrap();
+
+        assert_eq!(reader.get(&[key]).unwrap(), [val],);
+    }
+
+    /// Asserts that KEY is associated with VAL in DB's INDEX.
+    fn assert_index_key_value(index: &str, key: u8, val: u8, db: &LmdbDatabase) {
+        let reader = db.reader().unwrap();
+
+        assert_eq!(reader.index_get(index, &[key]).unwrap().unwrap(), [val],);
+    }
+
+    /// Asserts that KEY is not in DB.
+    fn assert_not_in_database_db(key: u8, db: &LmdbDatabase) {
+        let reader = db.reader().unwrap();
+
+        assert!(reader.get(&[key]).is_none());
+    }
+
+    /// Asserts that KEY is not in DB's INDEX.
+    fn assert_not_in_index(index: &str, key: u8, db: &LmdbDatabase) {
+        let reader = db.reader().unwrap();
+
+        assert!(reader.index_get(index, &[key]).unwrap().is_none());
+    }
+
+    fn create_context() -> Result<lmdb::LmdbContext, CliError> {
+        let path_config = config::get_path_config();
+        let statestore_path = &path_config.data_dir.join(config::get_filename());
+
+        lmdb::LmdbContext::new(statestore_path, 1, None)
+            .map_err(|err| CliError::EnvironmentError(format!("{}", err)))
+    }
+
+    #[test]
+    fn test_state_store_get() {
+
+        let mut ctx = create_context().unwrap();
+        let statestore_db = lmdb::LmdbDatabase::new(
+            &ctx,
+            &["index_consensus_state"],
+        ).map_err(|err| CliError::EnvironmentError(format!("{}", err))).unwrap();
+
+        let mut state_store = ConsensusStateStore::new(statestore_db);
+
+        assert!(state_store.get(BlockId::from(vec![11])).is_err());
+        state_store.put( BlockId::from(vec![11]), ConsensusState::default() );
+
+        assert!(state_store.get(BlockId::from(vec![11])).is_ok());
+        //cleanup
+        state_store.delete( BlockId::from(vec![11]) );
+    }
+
+    #[test]
+    fn test_state_store_put() {
+
+        let mut ctx = create_context().unwrap();
+        let statestore_db = lmdb::LmdbDatabase::new(
+            &ctx,
+            &["index_consensus_state"],
+        ).map_err(|err| CliError::EnvironmentError(format!("{}", err))).unwrap();
+
+        let mut state_store = ConsensusStateStore::new(statestore_db);
+
+        assert!(state_store.get(BlockId::from(vec![13])).is_err());
+        state_store.put( BlockId::from(vec![13]), ConsensusState::default() );
+
+        assert!(state_store.get(BlockId::from(vec![13])).is_ok());
+        assert_eq!(*state_store.get(BlockId::from(vec![13])).unwrap(),
+            ConsensusState::default());
+        //cleanup
+        state_store.delete( BlockId::from(vec![13]) );
+    }
+
+    #[test]
+    fn test_state_store_delete() {
+
+        let mut ctx = create_context().unwrap();
+        let statestore_db = lmdb::LmdbDatabase::new(
+            &ctx,
+            &["index_consensus_state"],
+        ).map_err(|err| CliError::EnvironmentError(format!("{}", err))).unwrap();
+
+        let mut state_store = ConsensusStateStore::new(statestore_db);
+
+        state_store.put( BlockId::from(vec![14]), ConsensusState::default() );
+        state_store.put( BlockId::from(vec![15]), ConsensusState::default() );
+
+        assert_eq!(*state_store.get(BlockId::from(vec![14])).unwrap(),
+            ConsensusState::default());
+        assert_eq!(*state_store.get(BlockId::from(vec![15])).unwrap(),
+            ConsensusState::default());
+
+        state_store.delete( BlockId::from(vec![14]) );
+        assert!(state_store.get(BlockId::from(vec![14])).is_err());
+        assert!(state_store.get(BlockId::from(vec![15])).is_ok());
+        //cleanup
+        state_store.delete( BlockId::from(vec![15]) );
     }
 }
